e87557d4ea6b8bf8f17817507ff2497ed23b72fc
likaik <mks032945@gmail.com>
2025-05-09 21:27:50 +0900

    5/09 Com

--- UnityXRLeapMotion/Assets/01Scripts/Demolisher/DemolisherController.cs
+++ UnityXRLeapMotion/Assets/01Scripts/Demolisher/DemolisherController.cs
@@ -17,82 +17,52 @@ public class DemolisherController : MonoBehaviour
         instance = this;
     }
 
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            GeneratePointAndDemolish(targetObject, 10, 0.5f);
-        }
-    }
+    //private void Update()
+    //{
+    //    if (Input.GetKeyDown(KeyCode.A))
+    //    {
+    //        GeneratePointAndDemolish(targetObject, 10, 0.5f);
+    //    }
+    //}
 
-    public void GeneratePointAndDemolish(GameObject targetObject, int pointCount, float demolishScale)
-    {
-        Debug.Log("Hi");
-
-        GameObject clone = new GameObject();
-        clone.name = "RockDemolished";
-        GameObject pointClone = new GameObject();
-        pointClone.name = "Point";
-        GameObject resultClone = new GameObject();
-        resultClone.name = "Result";
-
-        pointClone.transform.SetParent(clone.transform);
-        resultClone.transform.SetParent(clone.transform);
-
-        _pointGenerator.domainGameObject = targetObject;
-        _pointGenerator.pointCount = pointCount;
-        _pointGenerator.pointsParent = pointClone.transform;
-
-        _pointGenerator.Generate();
-
-        pointClone.SetActive(false);
-
-        Material mat = targetObject.GetComponent<MeshRenderer>().material;
-
-        _demolisher.TargetGameObject = targetObject;
-        _demolisher.ResultScale = demolishScale;
-        _demolisher.BreakPointsParent = pointClone.transform;
-        _demolisher.ResultParent = resultClone.transform;
-        _demolisher.InteriorMaterial = mat;
-
-        _demolisher.Demolish();
-
-        //SoftRock 컴퍼넌트 부착
-        for (int i = 0; i < resultClone.transform.childCount; i++)
-        {
-            GameObject piece = resultClone.transform.GetChild(i).gameObject;
-
-            piece.AddComponent<SoftRockModel>();
-        }
-    }
-
-    private void AddModelByRockType(GameObject targetObject, GameObject resultClone)
-    {
-        for (int i = 0; i < resultClone.transform.childCount; i++)
-        {
-            GameObject piece = resultClone.transform.GetChild(i).gameObject;
-
-            piece.AddComponent<SoftRockModel>();
-        }
-
-        //RockModel rock = targetObject.GetComponent<RockModel>();
-        //
-        //bool isHard = rock is HardRockModel;
-        //bool isSoft = rock is SoftRockModel;
-        //
-        //for (int i = 0; i < resultClone.transform.childCount; i++)
-        //{
-        //    GameObject piece = resultClone.transform.GetChild(i).gameObject;
-        //
-        //    if (isHard)
-        //    {
-        //        piece.AddComponent<SoftRockModel>();
-        //    }
-        //    else if (isSoft)
-        //    {
-        //        piece.AddComponent<SoilModel>();
-        //    }
-        //}
-
-    }
+    //public void GeneratePointAndDemolish(GameObject targetObject, int pointCount, float demolishScale)
+    //{
+    //    Debug.Log("Hi");
+    //
+    //    GameObject clone = new GameObject();
+    //    clone.name = "RockDemolished";
+    //    GameObject pointClone = new GameObject();
+    //    pointClone.name = "Point";
+    //    GameObject resultClone = new GameObject();
+    //    resultClone.name = "Result";
+    //
+    //    pointClone.transform.SetParent(clone.transform);
+    //    resultClone.transform.SetParent(clone.transform);
+    //
+    //    _pointGenerator.domainGameObject = targetObject;
+    //    _pointGenerator.pointCount = pointCount;
+    //    _pointGenerator.pointsParent = pointClone.transform;
+    //
+    //    _pointGenerator.Generate();
+    //
+    //    pointClone.SetActive(false);
+    //
+    //    Material mat = targetObject.GetComponent<MeshRenderer>().material;
+    //
+    //    _demolisher.TargetGameObject = targetObject;
+    //    _demolisher.ResultScale = demolishScale;
+    //    _demolisher.BreakPointsParent = pointClone.transform;
+    //    _demolisher.ResultParent = resultClone.transform;
+    //    _demolisher.InteriorMaterial = mat;
+    //
+    //    _demolisher.Demolish();
+    //
+    //    //SoftRock 컴퍼넌트 부착
+    //    for (int i = 0; i < resultClone.transform.childCount; i++)
+    //    {
+    //        GameObject piece = resultClone.transform.GetChild(i).gameObject;
+    //
+    //        piece.AddComponent<SoftRockModel>();
+    //    }
+    //}
 }
---
+++ UnityXRLeapMotion/Assets/01Scripts/Demolisher/RockModels/BoneModel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BoneModel : MonoBehaviour
+{
+    [SerializeField] private int _hitCount;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Test")
+        {
+            _hitCount++;
+            if (_hitCount == 5)
+            {
+                //CreateSoils();
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
