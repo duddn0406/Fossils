32dd91261a40eee70e0ffefdf018c84704af7d22
Jungeunwoo <129839712+Jungeunwoo@users.noreply.github.com>
2025-05-27 21:43:39 +0900

    Point Model
    포인트 데이터에 값 넣음 (result 추가)

--- UnityXRLeapMotion/Assets/01Scripts/MainSceneScripts/UI/Point/PointData.cs
+++ UnityXRLeapMotion/Assets/01Scripts/MainSceneScripts/UI/Point/PointData.cs
@@ -3,9 +3,17 @@
 [System.Serializable]
 public class PointData
 {
-    [SerializeField] private Sprite _sprite;
-    [SerializeField] private string _description;
+    [SerializeField] private string _name;                  // 화석 이름
+    [SerializeField] private Sprite _sprite;                // 지역 배경 이미지
+    [SerializeField] private string _description;           // 지역 설명
 
+    [SerializeField] private Sprite _resultSprite;          // 결과(화석) 이미지
+    [SerializeField] private string _resultDescription;     // 결과 설명
+
+    public string Name => _name;
     public Sprite Sprite => _sprite;
     public string Description => _description;
+
+    public Sprite ResultSprite => _resultSprite;
+    public string ResultDescription => _resultDescription;
 }
